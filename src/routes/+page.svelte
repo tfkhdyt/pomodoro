38473e13f8666b781fa9a47f6077ed8b0fa85b67
tfkhdyt <tfkhdyt@proton.me>
2024-04-09 05:31:21 +0700

    feat: show notification

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
 	import { match } from 'ts-pattern';
+	import {
+		isPermissionGranted,
+		requestPermission,
+		sendNotification
+	} from '@tauri-apps/api/notification';
+	import { appWindow } from '@tauri-apps/api/window';
 
 	const time = {
-		pomodoro: 25,
-		'short-break': 5,
-		'long-break': 15
+		pomodoro: 0.3,
+		'short-break': 0.1,
+		'long-break': 0.2
 	};
 
 	let type: 'pomodoro' | 'short-break' | 'long-break' = 'pomodoro';
@@ -18,6 +24,32 @@
 		.padStart(2, '0')}:${(timeLeft % 60).toString().padStart(2, '0')}`;
 
 	let intervalId: number;
+
+	let permissionGranted: boolean;
+	(async () => {
+		permissionGranted = await isPermissionGranted();
+	})();
+
+	$: if (!permissionGranted) {
+		askNotifPermission();
+	}
+
+	$: (async () => {
+		switch (type) {
+			case 'pomodoro':
+				await appWindow.setTitle('Time to focus!');
+				break;
+			case 'short-break':
+			case 'long-break':
+				await appWindow.setTitle('Time for a break!');
+				break;
+		}
+	})();
+
+	async function askNotifPermission() {
+		const permission = await requestPermission();
+		permissionGranted = permission === 'granted';
+	}
 
 	function updateTimer() {
 		timeLeft--;
@@ -34,15 +66,22 @@
 		}
 	}
 
-	function nextStep() {
+	async function nextStep() {
+		if (!permissionGranted) {
+			await askNotifPermission();
+		}
+
 		clearInterval(intervalId);
 		buttonState = 'paused';
 		if (type === 'pomodoro' && reps % 4 !== 0) {
+			sendNotification({ title: 'Time to take a short break!' });
 			type = 'short-break';
 		} else if (type === 'pomodoro' && reps % 4 === 0) {
+			sendNotification({ title: 'Time to take a long break!' });
 			type = 'long-break';
 		} else {
 			reps++;
+			sendNotification({ title: 'Time to focus!' });
 			type = 'pomodoro';
 		}
 	}
