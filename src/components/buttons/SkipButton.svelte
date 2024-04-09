61e6d7669792d780e2cd546e41fe56b9d036576a
tfkhdyt <tfkhdyt@proton.me>
2024-04-09 21:49:57 +0700

    style: add transition to settings and skip buttons
    Adds a transition to the settings and skip buttons to make them more visually appealing.

--- src/components/buttons/SkipButton.svelte
+++ src/components/buttons/SkipButton.svelte
@@ -8,7 +8,7 @@
 <button
 	type="button"
 	on:click={nextStep}
-	class="absolute inset-y-5 right-14 focus:outline-none rounded-full h-fit p-2 dark hover:bg-background/15"
+	class="absolute inset-y-5 right-14 focus:outline-none rounded-full h-fit p-2 dark hover:bg-background/15 transition"
 	transition:fade={{ duration: 200 }}
 >
 	<SkipForwardIcon class="h-8 w-8" />
