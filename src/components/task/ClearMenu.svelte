3cfa04790158d410d80c9c3182eedde628b3c679
tfkhdyt <tfkhdyt@proton.me>
2024-04-18 06:45:05 +0700

    feat(task): Add clear menu with clear finished tasks
    Add the ability to clear finished tasks, reset act pomodoros, and clear all tasks from the task list.

--- src/components/task/ClearMenu.svelte
+++ src/components/task/ClearMenu.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
 	import * as DropdownMenu from '@/components/ui/dropdown-menu';
+	import type { Task } from '@/types';
+	import { confirm } from '@tauri-apps/api/dialog';
+	import { sendNotification } from '@tauri-apps/api/notification';
 	import {
 		EllipsisVerticalIcon,
 		ListChecksIcon,
@@ -7,12 +10,14 @@
 		Trash2Icon
 	} from 'lucide-svelte';
 	import { Button } from '../ui/button';
-	import type { Task } from '@/types';
-	import { confirm } from '@tauri-apps/api/dialog';
-	import { sendNotification } from '@tauri-apps/api/notification';
 
 	export let tasks: Task[];
 	export let save: () => Promise<void>;
+
+	$: canClearFinishedTasks = tasks.some((t) => t.done);
+	$: canResetActPomodoros =
+		tasks.length > 0 && tasks.map((t) => t.act).reduce((a, b) => a + b) > 0;
+	$: canClearAllTasks = tasks.length > 0;
 
 	async function clearFinishedTasks() {
 		const confirmed = await confirm(
@@ -26,6 +31,35 @@
 			sendNotification('Finished tasks has been cleared');
 		}
 	}
+
+	async function resetActPomodoros() {
+		const confirmed = await confirm(
+			'Are you sure want to reset all act pomodoros count?'
+		);
+
+		if (confirmed) {
+			tasks = tasks.map((t) => ({
+				...t,
+				act: 0
+			}));
+
+			await save();
+
+			sendNotification('All act pomodoros count has been reset');
+		}
+	}
+
+	async function clearAllTasks() {
+		const confirmed = await confirm('Are you sure want to clear all tasks?');
+
+		if (confirmed) {
+			tasks = [];
+
+			await save();
+
+			sendNotification('All tasks has been cleared');
+		}
+	}
 </script>
 
 <DropdownMenu.Root>
@@ -36,16 +70,22 @@
 	</DropdownMenu.Trigger>
 	<DropdownMenu.Content>
 		<DropdownMenu.Group>
-			<DropdownMenu.Item on:click={clearFinishedTasks} class="cursor-pointer">
+			<DropdownMenu.Item
+				disabled={!canClearFinishedTasks}
+				on:click={clearFinishedTasks}
+				class="cursor-pointer">
 				<ListChecksIcon class="w-4 h-4 mr-2" />
 				Clear finished tasks
 			</DropdownMenu.Item>
-			<DropdownMenu.Item>
+			<DropdownMenu.Item
+				disabled={!canResetActPomodoros}
+				on:click={resetActPomodoros}
+				class="cursor-pointer">
 				<RotateCcwIcon class="w-4 h-4 mr-2" />
 				Reset act pomodoros
 			</DropdownMenu.Item>
 			<DropdownMenu.Separator />
-			<DropdownMenu.Item>
+			<DropdownMenu.Item disabled={!canClearAllTasks} on:click={clearAllTasks}>
 				<Trash2Icon class="w-4 h-4 mr-2" />
 				Clear all tasks
 			</DropdownMenu.Item>
