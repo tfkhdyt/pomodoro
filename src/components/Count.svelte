b52b112fa39a30ac92d59f098832081fcdb82477
tfkhdyt <tfkhdyt@proton.me>
2024-04-18 09:56:56 +0700

    feat: show current active task title in window title

--- src/components/Count.svelte
+++ src/components/Count.svelte
@@ -1,27 +1,43 @@
 <script lang="ts">
+	import { appWindow } from '@tauri-apps/api/window';
 	import { match } from 'ts-pattern';
-	import type { PomodoroType } from '@/types';
 	import type { LayoutData } from '../routes/$types';
 
 	export let data: LayoutData;
 	export let resetReps: () => Promise<void>;
 	export let reps: number;
-	export let pomodoroType: PomodoroType;
 
-	$: currentTaskTitle = data.appData.tasks.find((it) => it.id === data.appData.activeTask)?.title;
+	$: currentTaskTitle = data.appData.tasks.find(
+		(it) => it.id === data.appData.activeTask
+	)?.title;
+
+	$: (async () => {
+		switch (data.appData.pomodoroState) {
+			case 'pomodoro':
+				await appWindow.setTitle(
+					`${currentTaskTitle ?? 'Time to focus!'} — Pomodoro`
+				);
+				break;
+			case 'short-break':
+			case 'long-break':
+				await appWindow.setTitle(
+					`${currentTaskTitle ?? 'Time for a break!'} — Pomodoro`
+				);
+				break;
+		}
+	})();
 </script>
 
 <div class="md:text-lg mt-4">
 	<button
 		type="button"
 		on:click={resetReps}
-		class="text-gray-300 hover:text-white focus:outline-none"
-	>
+		class="text-gray-300 hover:text-white focus:outline-none">
 		#{reps}
 	</button>
 	<h3 class="font-medium select-none cursor-default">
 		{currentTaskTitle ??
-			match(pomodoroType)
+			match(data.appData.pomodoroState)
 				.with('pomodoro', () => 'Time to focus!')
 				.otherwise(() => 'Time for a break!')}
 	</h3>
